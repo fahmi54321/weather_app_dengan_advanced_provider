ff7af89c914dc4b43976254b5fa788dacfb83e46
unknown <fahmiabdulaziz44@gmail.com>
2022-11-09 15:38:06 +0700

    create weather provider

--- lib/providers/weather/weather_provider.dart
+++ lib/providers/weather/weather_provider.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
@@ -9,17 +8,33 @@ import 'package:weather_app_dengan_advanced_provider/repository/weather_reposito
 part 'weather_state.dart';
 
 class WeatherProvider with ChangeNotifier {
+  // todo 7
   WeatherState _state = WeatherState.initial();
-
   WeatherState get state => _state;
 
+  // todo 8
   final WeatherRepository weatherRepository;
   WeatherProvider({
     required this.weatherRepository,
   });
 
+  // todo 9 method api (finish)
   Future<void> fetchWather(String city) async {
     _state = _state.copyWith(status: WeatherStatus.loading);
     notifyListeners();
+
+    try {
+      final Weather weather = await weatherRepository.fetchWeather(city);
+
+      _state = _state.copyWith(status: WeatherStatus.loaded);
+
+      notifyListeners();
+    } on CustomError catch (e) {
+      _state = _state.copyWith(
+        status: WeatherStatus.error,
+        customError: e,
+      );
+      notifyListeners();
+    }
   }
 }
--- lib/providers/weather/weather_state.dart
+++ lib/providers/weather/weather_state.dart
@@ -1,5 +1,6 @@
 part of 'weather_provider.dart';
 
+//todo 1 create status
 enum WeatherStatus {
   initial,
   loading,
@@ -8,6 +9,7 @@ enum WeatherStatus {
 }
 
 class WeatherState extends Equatable {
+  //todo 2
   final WeatherStatus status;
   final Weather weather;
   final CustomError customError;
@@ -17,6 +19,7 @@ class WeatherState extends Equatable {
     required this.customError,
   });
 
+  // todo 6 initial (next weather_provider.dart)
   factory WeatherState.initial() {
     return WeatherState(
       status: WeatherStatus.initial,
@@ -25,12 +28,15 @@ class WeatherState extends Equatable {
     );
   }
 
+  // todo 3 equatable
   @override
   List<Object> get props => [status, weather, customError];
 
+  // todo 4 toString
   @override
   bool get stringify => true;
 
+  // todo 5 copyWith
   WeatherState copyWith({
     WeatherStatus? status,
     Weather? weather,
