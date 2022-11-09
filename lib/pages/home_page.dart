6788979209765df76f8a98344451a6f03754092e
fahmi54321 <fahmiabdulaziz44@gmail.com>
2022-11-09 21:28:22 +0700

    implement weather provider

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:weather_app_dengan_advanced_provider/pages/search_page.dart';
 import 'package:weather_app_dengan_advanced_provider/providers/weather/weather_provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,18 +11,104 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  // todo 3
+  String? city;
+
+  //todo 5
+  late final WeatherProvider weatherProvider;
+
+  //todo 6
+  @override
+  void dispose() {
+    weatherProvider.removeListener(_registerListener);
+    super.dispose();
+  }
+
+  //todo 7
   @override
   void initState() {
     super.initState();
-    _fetchWeather();
+    weatherProvider = context.read<WeatherProvider>();
+    weatherProvider.addListener(_registerListener);
   }
 
-  // todo 5 (fins=ish)
-  _fetchWeather() {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      context.read<WeatherProvider>().fetchWather('london');
-    });
+  //todo 8
+  void _registerListener() {
+    final WeatherState state = context.read<WeatherProvider>().state;
+
+    if (state.status == WeatherStatus.error) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Text(state.customError.errMsg),
+          );
+        },
+      );
+    }
+  }
+
+  //todo 9 (finish)
+  Widget _showWeather() {
+    final state = context.watch<WeatherProvider>().state;
+
+    if (state.status == WeatherStatus.initial) {
+      return const Center(
+        child: Text(
+          'Select a city',
+          style: TextStyle(
+            fontSize: 20.0,
+          ),
+        ),
+      );
+    }
+
+    if (state.status == WeatherStatus.error && state.weather.name.isEmpty) {
+      return const Center(
+        child: Text(
+          'Select a city',
+          style: TextStyle(
+            fontSize: 20.0,
+          ),
+        ),
+      );
+    }
+
+    if (state.status == WeatherStatus.loading) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    return ListView(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height / 6,
+        ),
+        Text(
+          state.weather.name,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontSize: 40.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 10.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              TimeOfDay.fromDateTime(state.weather.lastUpdated).format(context),
+              style: const TextStyle(fontSize: 18.0),
+            ),
+            const SizedBox(width: 10.0),
+            Text(
+              '(${state.weather.country})',
+              style: const TextStyle(fontSize: 18.0),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 
   @override
@@ -29,10 +116,29 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Weather'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              //todo 1 (next search_page.dart)
+              city = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SearchPage(),
+                ),
+              );
+
+              // todo 3
+              if (city != null) {
+                context.read<WeatherProvider>().fetchWather(city ?? '');
+              }
+            },
+            icon: Icon(
+              Icons.search,
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        child: Text('Home'),
-      ),
+      body: _showWeather(), //todo 4
     );
   }
 }
--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -8,14 +8,71 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  final formKey = GlobalKey<FormState>();
+  String? city;
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  //todo 2 (next home_page.dart)
+  void _submit() {
+    setState(() {
+      autovalidateMode = AutovalidateMode.always;
+    });
+
+    final form = formKey.currentState;
+
+    if (form != null && form.validate()) {
+      form.save();
+      Navigator.pop(
+        context,
+        city?.trim(),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Search'),
       ),
-      body: Center(
-        child: Text('Search'),
+      body: Form(
+        key: formKey,
+        autovalidateMode: autovalidateMode,
+        child: Column(
+          children: [
+            const SizedBox(height: 60.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18.0),
+              child: TextFormField(
+                autofocus: true,
+                style: const TextStyle(fontSize: 18.0),
+                decoration: const InputDecoration(
+                  labelText: 'City name',
+                  hintText: 'more than 2 characters',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(),
+                ),
+                validator: (String? input) {
+                  if (input == null || input.trim().length < 2) {
+                    return 'City name must be at least 2 characters long';
+                  }
+                  return null;
+                },
+                onChanged: (String? input) {
+                  city = input;
+                },
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: _submit,
+              child: const Text(
+                'How is weather?',
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
